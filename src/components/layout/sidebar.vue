7f8eea01e8d40f2c3ddacc2c22deada6851f4b42
GinMu <smu@anfengde.com>
2016-07-16 01:30:10 +0800

    add sidebar click event

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -3,12 +3,36 @@
 		<div class="row">
 			<div class="col-sm-3 col-md-2 sidebar">
 				<ul class="nav nav-sidebar">
-					<li class="active" type="number"><a href="#">数据数量</a></li>
-					<li type="percent"><a href="#">数据占比</a></li>
-					<li type="region"><a href="#">各省数据</a></li>
-					<li type="uuid"><a href="#">UUID查询</a></li>
+					<li v-for="tab in tabs" :class="{'active':$index===selected}" @click="choose($index)" type={{tab.tabType}}><a href="javascript:void(0)">{{tab.tabName}}</a></li>
 				</ul>
 			</div>
 		</div>
 	</div>
 </template>
+<script>
+	export default {
+		data() {
+				return {
+					tabs: [{
+						tabName: '数据数量',
+						tabType: 'number'
+					}, {
+						tabName: '数据占比',
+						tabType: 'percent'
+					}, {
+						tabName: '各省数据',
+						tabType: 'region'
+					}, {
+						tabName: 'UUID查询',
+						tabType: 'uuid'
+					}],
+					selected: 0
+				}
+			},
+			methods: {
+				choose(index) {
+					this.selected = index;
+				}
+			}
+	}
+</script>
